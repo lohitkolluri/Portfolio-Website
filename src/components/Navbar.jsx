cf08dac09d54fff3b36ae8b490f963fa0cb3b6a5
lohit <lohitkolluri@gmail.com>
2023-06-15 15:05:06 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,9 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, SetActive] = useState("");
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav
       className={
@@ -41,7 +43,33 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => setActive(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
