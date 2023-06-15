c8fbc45cbb01a796f8c8ee8fbee455748c1fc2cc
lohit <lohitkolluri@gmail.com>
2023-06-15 18:46:09 +0530

    Hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
 
-export default Hero
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
