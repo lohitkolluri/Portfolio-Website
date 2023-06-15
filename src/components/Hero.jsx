a59bd1ea7aa321a9f2888ef42d04ed8f1e7556ae
lohit <lohitkolluri@gmail.com>
2023-06-15 22:27:01 +0530

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,59 @@
 import { motion } from "framer-motion";
+
 import { styles } from "../styles";
-import { TypeAnimation } from "react-type-animation";
+import { ComputersCanvas } from "./canvas";
+
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">lohit</span>
-            <p>
-              I do &nbsp;
-              <TypeAnimation
-                sequence={["CyberSecurity", 1000, "NLP", 1000, "Ai & ML", 1000]}
-                speed={50}
-                style={{ height: "10px" }}
-                repeat={Infinity}
-              />
-            </p>
+            Hi, I'm <span className="text-[#915EFF]">lohit</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I do
+            <Typewriter
+              options={{
+                strings: ["CyberSecurity", "Ai & ML", "NLP"],
+                autoStart: true,
+                loop: true,
+                loopCount: Infinity,
+                deleteSpeed: "natural",
+                pauseFor: 1000,
+              }}
+            />
+          </p>
         </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
