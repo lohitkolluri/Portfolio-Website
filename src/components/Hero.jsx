c54235e1fdb7971dc56502000457c7c50ad0be0d
lohit <lohitkolluri@gmail.com>
2023-06-15 18:58:56 +0530

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,15 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">lohit</span>
+            <h1 h1 className={`${styles.heroSubText} mt-2 text-white`}>
+              I am into 
+            </h1>
+          </h1>
         </div>
       </div>
     </section>
