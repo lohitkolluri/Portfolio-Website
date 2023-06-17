35f32838c857f6211d2b646aafa541b8ae8f216e
lohit <lohitkolluri@gmail.com>
2023-06-17 14:09:07 +0530

    Done with contact section.

--- src/constants/index.js
+++ src/constants/index.js
@@ -160,32 +160,32 @@ const experiences = [
   // },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
-];
+// const testimonials = [
+//   {
+//     testimonial:
+//       "XYZ",
+//     name: "John Doe",
+//     designation: "Human",
+//     company: "123 Enterprises",
+//     image: "Image Link",
+//   },
+//   {
+//     testimonial:
+//       "XYZ",
+//     name: "John Doe",
+//     designation: "Human",
+//     company: "456 Enterprises",
+//     image: Image Link",
+//   },
+//   {
+//     testimonial:
+//       "XYZ",
+//     name: "John Doe",
+//     designation: "Human",
+//     company: "789 Enterprises",
+//     image: "Image Link",
+//   },
+// ];
 
 const projects = [
   {
@@ -261,4 +261,10 @@ const projects = [
   },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export {
+  services,
+  technologies,
+  experiences,
+  // testimonials,
+  projects,
+};
