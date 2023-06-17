d375cfc0ab4af82114d4701eeb2267e72ba0a0db
lohit <lohitkolluri@gmail.com>
2023-06-17 12:57:47 +0530

    .

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Works
+export default SectionWrapper(Works, "");
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,265 +1,256 @@
 import {
-    c,
-    python,
-    java,
-    cpp,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
-    four,
-    Mathwork,
-    Cisco,
-    edunet,
-    carrent,
-    jobit,
-    tripguide,
-    threejs,
-  } from "../assets";
-  
-  export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-  
-  const services = [
-    {
-      title: "C",
-      icon: c,
-    },
-    {
-      title: "C++",
-      icon: cpp,
-    },
-    {
-      title: "Python",
-      icon: python,
-    },
-    {
-      title: "Java",
-      icon: java,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
-  ];
-  
-  const experiences = [
-    {
-      title: "Artificial Intelligence Intern",
-      company_name: "Edunet Foundation | IBM SkillsBuild | AICTE",
-      icon: edunet,
-      iconBg: "#ffffff",
-      date: "June 2023 - July 2023",
-      points: [
-        "1",
-        "2",
-        "3",
-        "4",
-      ],
-    },
-    // {
-    //   title: "SRC/CONSTANTS/EXPERIENCES ln:132",
-    //   company_name: "Cisco",
-    //   icon: Cisco,
-    //   iconBg: "#ffffff",
-    //   date: "NA 2023 - NA 2023",
-    //   points: [
-    //     "1",
-    //     "2",
-    //     "3",
-    //     "4",
-    //   ],
-    // },
-    // {
-    //   title: "SRC/CONSTANTS/EXPERIENCES ln:145",
-    //   company_name: "MathWorks",
-    //   icon: Mathwork,
-    //   iconBg: "#ffffff",
-    //   date: "NA 2023 - NA 2023",
-    //   points: [
-    //     "1",
-    //     "2",
-    //     "3",
-    //     "4",
-    //   ],
-    // },
-    // {
-    //   title: "SRC/CONSTANTS/EXPERIENCES ln:158",
-    //   company_name: "four",
-    //   icon: four,
-    //   iconBg: "#ffffff",
-    //   date: "NA 2023 - NA 2023",
-    //   points: [
-    //     "1",
-    //     "2",
-    //     "3",
-    //     "4",
-    //   ],
-    // },
-  ];
-  
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-  ];
-  
-  const projects = [
-    {
-      name: "Car Rent",
-      description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Job IT",
-      description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "scss",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: jobit,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Trip Guide",
-      description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-      tags: [
-        {
-          name: "nextjs",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "supabase",
-          color: "green-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
-    },
-  ];
-  
-  export { services, technologies, experiences, testimonials, projects };
+  c,
+  python,
+  java,
+  cpp,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  four,
+  Mathwork,
+  Cisco,
+  edunet,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "C",
+    icon: c,
+  },
+  {
+    title: "C++",
+    icon: cpp,
+  },
+  {
+    title: "Python",
+    icon: python,
+  },
+  {
+    title: "Java",
+    icon: java,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  // {
+  //   name: "TypeScript",
+  //   icon: typescript,
+  // },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Artificial Intelligence Intern",
+    company_name: "Edunet Foundation | IBM SkillsBuild | AICTE",
+    icon: edunet,
+    iconBg: "#ffffff",
+    date: "June 2023 - July 2023",
+    points: ["1", "2", "3", "4"],
+  },
+  // {
+  //   title: "SRC/CONSTANTS/EXPERIENCES ln:132",
+  //   company_name: "Cisco",
+  //   icon: Cisco,
+  //   iconBg: "#ffffff",
+  //   date: "NA 2023 - NA 2023",
+  //   points: [
+  //     "1",
+  //     "2",
+  //     "3",
+  //     "4",
+  //   ],
+  // },
+  // {
+  //   title: "SRC/CONSTANTS/EXPERIENCES ln:145",
+  //   company_name: "MathWorks",
+  //   icon: Mathwork,
+  //   iconBg: "#ffffff",
+  //   date: "NA 2023 - NA 2023",
+  //   points: [
+  //     "1",
+  //     "2",
+  //     "3",
+  //     "4",
+  //   ],
+  // },
+  // {
+  //   title: "SRC/CONSTANTS/EXPERIENCES ln:158",
+  //   company_name: "four",
+  //   icon: four,
+  //   iconBg: "#ffffff",
+  //   date: "NA 2023 - NA 2023",
+  //   points: [
+  //     "1",
+  //     "2",
+  //     "3",
+  //     "4",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
