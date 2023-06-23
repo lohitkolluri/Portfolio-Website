0e0534f9bc1f4f76ac7fec4ee532ed240d1a521c
lohit <lohitkolluri@gmail.com>
2023-06-23 18:36:08 +0530

    Added a resume window where we can display our resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,24 +1,66 @@
 import { motion } from "framer-motion";
 import resumePDF from "../assets/resume.pdf";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { useState } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Resume = ({ closeResume }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10; // Update with the total number of pages in your resume PDF
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-80 z-50">
       <motion.div
-        initial={{ y: "100%" }}
-        animate={{ y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="max-w-3xl bg-white rounded-lg p-6"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.3 }}
+        className="max-w-3xl bg-white rounded-lg p-6 relative"
+        style={{ width: "100%", height: "100%" }} 
       >
-        <div className="flex justify-end">
-          <button className="text-gray-600 hover:text-gray-900" onClick={closeResume}>
+        <div className="absolute top-2 right-2">
+          <button
+            className="text-gray-600 hover:text-gray-900"
+            onClick={closeResume}
+          >
             Close
           </button>
         </div>
-        <iframe src={resumePDF} title="Resume" className="w-full h-screen" />
+        <iframe
+          src={`${resumePDF}#page=${currentPage}`}
+          title="Resume"
+          className="w-full h-5/6 bg-white"
+        />
+        <div className="flex justify-center mt-4">
+          <button
+            className="bg-gray-200 text-gray-700 rounded-full p-2 mr-2"
+            onClick={handlePrevPage}
+          >
+            <MdKeyboardArrowLeft size={20} />
+          </button>
+          <button
+            className="bg-gray-200 text-gray-700 rounded-full p-2"
+            onClick={handleNextPage}
+          >
+            <MdKeyboardArrowRight size={20} />
+          </button>
+        </div>
       </motion.div>
     </div>
   );
 };
 
 export default Resume;
+
+
