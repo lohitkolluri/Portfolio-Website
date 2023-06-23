60d2222c31535a30e51c40c5a31bae5f7be0e9fc
lohit <lohitkolluri@gmail.com>
2023-06-23 20:22:56 +0530

    react-pdf was giving issue so using images instead of pdf

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,66 +1,99 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-import resumePDF from "../assets/resume.pdf";
+import image1 from "../assets/resume/1.jpg";
+import image2 from "../assets/resume/2.jpg";
+import image3 from "../assets/resume/3.jpg";
 
 const Resume = ({ closeResume }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 3; 
-  const pdfViewerRef = useRef(null);
+  const [currentImage, setCurrentImage] = useState(1);
+  const totalImages = 3; // Update with the total number of images
 
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+  const handlePrevImage = () => {
+    if (currentImage > 1) {
+      setCurrentImage(currentImage - 1);
     }
   };
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
+  const handleNextImage = () => {
+    if (currentImage < totalImages) {
+      setCurrentImage(currentImage + 1);
     }
   };
 
   const handleClickOutside = (event) => {
-    if (pdfViewerRef.current && !pdfViewerRef.current.contains(event.target)) {
+    if (event.target === event.currentTarget) {
       closeResume();
     }
   };
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
+
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
-  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-80 z-50">
+    <div
+      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+      onClick={handleClickOutside}
+    >
       <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 0.7 }}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 0.54 }}
         transition={{ duration: 0.3 }}
-        className="w-80 bg-white rounded-2xl p-6 relative"
-        ref={pdfViewerRef}
+        className="w-85 h-85 bg-white bg-opacity-60 rounded-2xl p-6 relative"
+        onClick={(event) => event.stopPropagation()}
       >
+        <div className="absolute top-2 right-2">
+          <button
+            className="text-gray-600 hover:text-gray-900"
+            onClick={closeResume}
+          >
+            Close
+          </button>
+        </div>
         <div className="flex flex-col justify-center items-center mb-4">
-          <div className="w-full rounded-xl overflow-hidden shadow-lg">
-            <Document
-              file={resumePDF}
-              onLoadError={(error) => console.log("PDF load error", error)}
-              renderMode="canvas"
-            >
-              <Page pageNumber={currentPage} width={300} />
-            </Document>
+          <div className="w-full h-full rounded-xl overflow-hidden shadow-lg">
+            {currentImage === 1 && (
+              <motion.img
+                src={image1}
+                alt="Image 1"
+                className="w-full h-full object-contain"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}
+              />
+            )}
+            {currentImage === 2 && (
+              <motion.img
+                src={image2}
+                alt="Image 2"
+                className="w-full h-full object-contain"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}
+              />
+            )}
+            {currentImage === 3 && (
+              <motion.img
+                src={image3}
+                alt="Image 3"
+                className="w-full h-full object-contain"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}
+              />
+            )}
           </div>
           <div className="flex justify-center mt-4">
             <motion.button
               className="bg-gray-200 text-gray-700 rounded-full p-2 mx-2"
-              onClick={handlePrevPage}
+              onClick={handlePrevImage}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
@@ -68,7 +101,7 @@ const Resume = ({ closeResume }) => {
             </motion.button>
             <motion.button
               className="bg-gray-200 text-gray-700 rounded-full p-2 mx-2"
-              onClick={handleNextPage}
+              onClick={handleNextImage}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
             >
@@ -76,17 +109,9 @@ const Resume = ({ closeResume }) => {
             </motion.button>
           </div>
         </div>
-        <div className="flex justify-center mt-4">
-          <button className="bg-blue-500 text-white rounded-lg px-4 py-2 mr-2">
-            Hire Me
-          </button>
-          <button className="bg-green-500 text-white rounded-lg px-4 py-2">
-            Download
-          </button>
-        </div>
       </motion.div>
     </div>
   );
 };
 
-export default Resume
+export default Resume;
