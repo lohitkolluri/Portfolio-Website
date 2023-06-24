2cd8f4f80333eae004900466c87d238911efb4ea
lohit <lohitkolluri@gmail.com>
2023-06-24 11:48:16 +0530

    added adnimation

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 import image1 from "../assets/resume/1.jpg";
@@ -38,104 +38,110 @@ const Resume = ({ closeResume }) => {
   }, []);
 
   return (
-    <div
-      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 z-50"
-      onClick={handleClickOutside}
-    >
+    <AnimatePresence>
       <motion.div
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{
-          opacity: 1,
-          scale: window.matchMedia("(min-width: 768px)").matches ? 0.525 : 1,
-        }}
-        transition={{ duration: 0.5, ease: "easeOut" }}
-        className="w-85 h-85 bg-white bg-opacity-90 rounded-2xl p-6 relative"
-        onClick={(event) => event.stopPropagation()}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 z-50"
+        onClick={handleClickOutside}
       >
-        {!window.matchMedia("(min-width: 768px)").matches && (
-          <div className="absolute top-2 right-2">
-            <button
-              className="text-gray-600 hover:text-gray-900"
-              onClick={closeResume}
-            >
-              Close
-            </button>
-          </div>
-        )}
-        <div className="flex flex-col justify-center items-center mb-4">
-          <div className="w-full h-full rounded-xl overflow-hidden shadow-lg">
-            {currentImage === 1 && (
-              <motion.img
-                src={image1}
-                alt="Image 1"
-                className="w-full h-full object-contain"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-              />
-            )}
-            {currentImage === 2 && (
-              <motion.img
-                src={image2}
-                alt="Image 2"
-                className="w-full h-full object-contain"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-              />
-            )}
-            {currentImage === 3 && (
-              <motion.img
-                src={image3}
-                alt="Image 3"
-                className="w-full h-full object-contain"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-              />
-            )}
-          </div>
-          <div className="flex justify-center mt-4">
-            <div className="flex items-center">
-              <a
-                href={resumePDF}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-green-500 text-white rounded-full p-2 px-4 hover:bg-green-600 transition-colors duration-200"
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{
+            opacity: 1,
+            scale: window.matchMedia("(min-width: 768px)").matches ? 0.525 : 1,
+          }}
+          transition={{ duration: 0.5 }}
+          className="w-85 h-85 bg-white bg-opacity-90 rounded-2xl p-6 relative"
+          onClick={(event) => event.stopPropagation()}
+        >
+          {!window.matchMedia("(min-width: 768px)").matches && (
+            <div className="absolute top-2 right-2">
+              <button
+                className="text-gray-600 hover:text-gray-900"
+                onClick={closeResume}
               >
-                Download
-              </a>
-              <motion.button
-                className="bg-green-500 text-white rounded-full p-2 mx-2"
-                onClick={handlePrevImage}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ duration: 0.2, ease: "easeInOut" }}
-              >
-                <FiChevronLeft size={35} />
-              </motion.button>
-              <motion.button
-                className="bg-green-500 text-white rounded-full p-2 mx-2"
-                onClick={handleNextImage}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ duration: 0.2, ease: "easeInOut" }}
-              >
-                <FiChevronRight size={35} />
-              </motion.button>
-              <a
-                href="http://website/#contact"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-green-500 text-white rounded-full p-2 px-4 ml-2 hover:bg-green-600 transition-colors duration-200"
-              >
-                Hire Me
-              </a>
+                Close
+              </button>
+            </div>
+          )}
+          <div className="flex flex-col justify-center items-center mb-4">
+            <div className="w-full h-full rounded-xl overflow-hidden shadow-lg">
+              {currentImage === 1 && (
+                <motion.img
+                  src={image1}
+                  alt="Image 1"
+                  className="w-full h-full object-contain"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                />
+              )}
+              {currentImage === 2 && (
+                <motion.img
+                  src={image2}
+                  alt="Image 2"
+                  className="w-full h-full object-contain"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                />
+              )}
+              {currentImage === 3 && (
+                <motion.img
+                  src={image3}
+                  alt="Image 3"
+                  className="w-full h-full object-contain"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                />
+              )}
+            </div>
+            <div className="flex justify-center mt-4">
+              <div className="flex items-center">
+                <a
+                  href={resumePDF}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-500 text-white rounded-full p-2 px-4 hover:bg-green-600 transition-colors duration-200"
+                >
+                  Download
+                </a>
+                <motion.button
+                  className="bg-green-500 text-white rounded-full p-2 mx-2"
+                  onClick={handlePrevImage}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <FiChevronLeft size={35} />
+                </motion.button>
+                <motion.button
+                  className="bg-green-500 text-white rounded-full p-2 mx-2"
+                  onClick={handleNextImage}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <FiChevronRight size={35} />
+                </motion.button>
+                <a
+                  href="http://website/#contact"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-500 text-white rounded-full p-2 px-4 ml-4 hover:bg-green-600 transition-colors duration-200"
+                >
+                  Hire Me
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </motion.div>
-    </div>
+    </AnimatePresence>
   );
 };
 
