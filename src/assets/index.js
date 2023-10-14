db42ae8204e962cc07fb92d480c0156f807c7016
Lohit Kolluri <lohitkolluri@Lohits-MacBook-Pro.local>
2023-10-14 18:48:40 +0530

    Compress Image and Sorted Assets 💨

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,65 +1,55 @@
-import python from "./python.png";
-import c from "./c.png";
-import cpp from "./cpp.png";
+import mathwork from "./company/mathwork.png";
+import edunet from "./company/edunet.png";
+
+import python from "./tech-stack/python.png";
+import c from "./tech-stack/c.png";
+import cpp from "./tech-stack/cpp.png";
+import java from "./tech-stack/java.png";
+
+import weatherpedia from "./projects/weatherpedia.png";
+import termpw from "./projects/termpw.png";
+import payloadmaster from "./projects/payloadmaster.png";
+import mhft from "./projects/mhft.png";
+import sketcher from "./projects/sketcher.png";
+import CompileVortex from "./projects/CompileVortex.png";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.png";
 
 import logo from "./logo.svg";
-import java from "./java.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
-import edunet from "./company/edunet.png";
-import mathwork from "./company/mathwork.png";
-
-import weatherpedia from "./WeatherPedia.png";
-import termpw from "./termpw.png";
-import payloadmaster from "./payloadmaster.png";
-import mhft from "./mhft.png";
-import sketcher from "./sketcher.png";
-import CompileVortex from "./CompileVortex.png";
-
 export {
-  logo,
+  mathwork,
+  edunet,
   python,
-  java,
-  cpp,
   c,
+  cpp,
+  java,
   github,
-  menu,
-  close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
   threejs,
-  edunet,
   weatherpedia,
   termpw,
   payloadmaster,
   mhft,
   sketcher,
   CompileVortex,
-  mathwork
+  logo,
+  menu,
+  close,
 };
