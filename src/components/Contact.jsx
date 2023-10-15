e52aa8c452976f0c8a8951fa7a2f80edeac2267e
Lohit Kolluri <lohitkolluri@Lohits-MacBook-Pro.local>
2023-10-16 00:58:43 +0530

    Add form validation and confirmation message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,20 +28,40 @@ const Contact = () => {
     email: "",
     message: "",
   });
-
   const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [confirmation, setConfirmation] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
     setForm({
       ...form,
       [name]: value,
     });
   };
 
+  const validateEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setEmailError("");
+    setNameError("");
+    setConfirmation("");
+
+    if (!validateEmail(form.email)) {
+      setEmailError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!form.name.trim()) {
+      setNameError("Name is required.");
+      return;
+    }
+
     setLoading(true);
 
     emailjs
@@ -60,57 +80,57 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("I will get back to you as soon as possible.");
+          setConfirmation("Thank you! I will get back to you as soon as possible.");
 
           setForm({
             name: "",
             email: "",
             message: "",
           });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-
-          alert("Something went wrong. Please try again. :/");
         }
-      );
+      )
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+        setConfirmation("Something went wrong. Please try again. :/");
+      });
   };
 
   return (
     <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact Me</h3>
 
         <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
-        <InputField
-  label="Your Name"
-  name="name"
-  value={form.name}
-  onChange={handleChange}
-  placeholder="Insert Your name here..."
-  type="text"
-/>
+          <InputField
+            label="Your Name"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            placeholder="Insert Your name here..."
+            type="text"
+          />
+          {nameError && <span className="text-red-500">{nameError}</span>}
 
-<InputField
-  label="Email Address"
-  name="email"
-  value={form.email}
-  onChange={handleChange}
-  placeholder="What's your email address?"
-  type="email"
-/>
+          <InputField
+            label="Email Address"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="What's your email address?"
+            type="email"
+          />
+          {emailError && <span className="text-red-500">{emailError}</span>}
 
-<InputField
-  label="Your Message"
-  name="message"
-  value={form.message}
-  onChange={handleChange}
-  placeholder="What you want to say...?"
-  type="text"
-/>
-
+          <InputField
+            label="Your Message"
+            name="message"
+            value={form.message}
+            onChange={handleChange}
+            placeholder="What you want to say...?"
+            type="text"
+          />
 
           <button
             type="submit"
@@ -118,6 +138,7 @@ const Contact = () => {
           >
             {loading ? "Sending..." : "Send"}
           </button>
+          {confirmation && <p className="text-green-500">{confirmation}</p>}
         </form>
       </motion.div>
 
