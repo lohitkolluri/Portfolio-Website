0cab025d89c9b364f2f2f4867728f2acd3a5b437
Lohit Kolluri <lohitkolluri@gmail.com>
2025-01-01 13:21:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         xs: '450px',
       },
       backgroundImage: {
-        'hero-pattern': `url(herobg.png)`,
+        'hero-pattern': `url(/herobg.png)`,
       },
     },
   },
